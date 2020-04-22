4057c98ea2536e8b0e5c2e35e577765fe718b4ff
Ryan Baker <ryan.andrew.baker@outlook.com>
2020-04-21 21:04:05 -0500

    Test

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"flag"
-	"time"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/golang/glog"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "hello!")
+}
 
 func main() {
 	var parameters WhSvrParameters
 
 	// get command line parameters
-	flag.IntVar(&parameters.port, "port", 443, "Webhook server port.")
+	flag.IntVar(&parameters.port, "port", 8080, "Webhook server port.")
 	flag.StringVar(&parameters.certFile, "tlsCertFile", "/etc/webhook/certs/cert.pem", "File containing the x509 Certificate for HTTPS.")
 	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/webhook/certs/key.pem", "File containing the x509 private key to --tlsCertFile.")
 	flag.Parse()
@@ -23,8 +32,8 @@ func main() {
 
 	// whsvr := &WebhookServer{
 	// 	server: &http.Server{
-	// 		Addr:      fmt.Sprintf(":%v", parameters.port),
-	// 		TLSConfig: &tls.Config{Certificates: []tls.Certificate{pair}},
+	// 		Addr: fmt.Sprintf(":%v", parameters.port),
+	// 		//TLSConfig: &tls.Config{Certificates: []tls.Certificate{pair}},
 	// 	},
 	// }
 
@@ -33,21 +42,23 @@ func main() {
 	// mux.HandleFunc("/mutate", whsvr.serve)
 	// whsvr.server.Handler = mux
 
-	// // start webhook server in new routine
-	// go func() {
-	// 	if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
-	// 		glog.Errorf("Failed to listen and serve webhook server: %v", err)
-	// 	}
-	// }()
+	// start webhook server in new routine
+	go func() {
+		//if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
+		// if err := whsvr.server.ListenAndServe(); err != nil {
+		// 	glog.Errorf("Failed to listen and serve webhook server: %v", err)
+		// }
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(":8080", nil)
+	}()
 
 	glog.Info("Server started")
-	time.Sleep(30000 * time.Second)
-	glog.Info("Sleep existed")
-	// listening OS shutdown singal
-	// signalChan := make(chan os.Signal, 1)
-	// signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	// <-signalChan
 
-	// glog.Infof("Got OS shutdown signal, shutting down webhook server gracefully...")
+	// handle interuptions
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	glog.Infof("Got OS shutdown signal, shutting down webhook server gracefully...")
 	//whsvr.server.Shutdown(context.Background())
 }
